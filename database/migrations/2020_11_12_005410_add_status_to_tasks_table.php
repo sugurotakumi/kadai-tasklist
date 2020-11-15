568cf2db8297179f5e310926ae7aa655ca172cca
takumi.suguro <takum070601@gmail.com>
2020-11-15 08:59:34 +0000

    add column

--- database/migrations/2020_11_12_005410_add_status_to_tasks_table.php
+++ database/migrations/2020_11_12_005410_add_status_to_tasks_table.php
@@ -14,8 +14,7 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status');
-            $table->char('name', 10);
+            $table->text('content');
         });
     }
 
